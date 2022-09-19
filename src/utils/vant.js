23cd8362c552a3d9946f1598278328d99ac286f9
lch <1161630698@qq.com>
2022-09-19 16:15:02 +0800

    抽离vant到一个文件中，让main更加清晰

---
+++ src/utils/vant.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import {
+  Icon,
+  Col,
+  Row,
+  NavBar,
+  Tabbar,
+  TabbarItem,
+  Cell,
+  CellGroup,
+  Search,
+  PullRefresh,
+  Toast,
+  List,
+} from "vant";
+import { Image as VanImage } from "vant";
+
+Vue.use(VanImage);
+Vue.use(NavBar);
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
+Vue.use(Col);
+Vue.use(Row);
+Vue.use(Cell);
+Vue.use(CellGroup);
+Vue.use(Icon);
+Vue.use(Search);
+Vue.use(PullRefresh);
+Vue.use(Toast);
+Vue.use(List);
